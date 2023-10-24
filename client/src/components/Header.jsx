4f38cf534142d1373153a3d18a62e7bd85176468
William Jiang <williamjiang26@10-209-64-57.dynapool.shanghai.nyu.edu>
2023-10-24 11:05:13 +0800

    imported app

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
+  return (
+
+      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+        <Link to='/'>
+          <h1 className='font-bold'>APP Name</h1>
+        </Link>
+          <ul className='flex gap-4'>
+          <div className='flex justify-between items-center max-w-6xl mx-auto p-3 highlightTextIn container'>
+            <Link to='/'>
+                <li><a alt='Home'>Home</a></li>
+              </Link>
+              <Link to='/about'>
+                <li><a alt='About'>About</a></li>
+              </Link>
+              <Link to='/bdi'>
+                <li><a alt='BDI'>BDI</a></li>
+              </Link>
+              <Link to='/profile'>
+                {currentUser ? (
+                  <img src={currentUser.profilePicture} alt='profile' className='h-7 w-7 rounded-full object-cover' />
+                ) : (
+                  <li><a alt='Sign In'>Sign In</a></li>
+                )}
+              </Link>
+          </div>
+          </ul>
+      </div>
+
+  );
+}
