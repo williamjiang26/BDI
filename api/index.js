dbdd192d9ddba8b8970f559b02f118aa064b1311
William Jiang <williamjiang26@10-209-64-57.dynapool.shanghai.nyu.edu>
2023-10-27 13:03:00 +0800

    added graph

--- api/index.js
+++ api/index.js
@@ -6,8 +6,9 @@ import authRoutes from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
 dotenv.config();
+console.log(process.env.MONGO)
 mongoose
-  .connect(process.env.MONGO)
+  .connect('mongodb+srv://wj2065:Dinwiddie26@cluster0.zqepzzf.mongodb.net/?retryWrites=true&w=majority')
   .then(() => {
     console.log('Connected to MongoDB');
   })
