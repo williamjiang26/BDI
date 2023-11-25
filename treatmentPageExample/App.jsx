37f7fc9aef852aeb1aa32f7f5945ea03f1447d12
Emilio Moya <54635784+emoya620@users.noreply.github.com>
2023-11-25 13:37:26 +0800

    Added an example of a treatment page structure

---
+++ treatmentPageExample/App.jsx
@@ -0,0 +1,62 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+        <h1 class="header"> Mild Mood Disturbances </h1>
+        <h2 class="header"> Based on your most recent BDI score, you most likely have a mild level of depression </h2>
+
+        <br/>
+        <p class="treatment"> Below are some resources that we hope will be valuable to you </p>
+        <p class="treatment"> 1) Psychoeducation </p>
+
+        <p class="description"> 
+            When one is trying to improve their mental health, it can 
+            be incredibly useful to get a better understanding related to one's
+            condition. That is why psychotherapy can be helpful in your journey
+            towards having improved mental health. 
+
+            <br/>
+            <br/>
+            Below are examples of some Psychoeducation are listed below:
+            <ul>
+                <li> Patients oftentimes feel worse when they initially start therapy, and this is a completely normal feeling </li>
+                <li> Educating oneself on the unique symptoms and aspects of depression </li>
+                <li> Consider joining a A psychoeducational support group that provides weekly group sessions where education, tools, and resources for navigating a specific diagnosis are shared </li>
+            </ul>
+            <br/>
+
+            <p> These are just a few examples of Psychoeducation. If you are interested in 
+                in-depth Psychoeducation, then consider following this link for an <a href="https://www.choosingtherapy.com/best-online-therapy/">online therapy platform</a>.     
+            </p>
+            
+        </p>
+        
+        <br/>
+        <p class="treatment"> 2) Additional Assessment Tools </p>
+        
+        <p class="description"> 
+            If you are here, then chances are you were looking for assessment tools that would help you get a better understanding of your current mental state.
+            It is very important to remember that mental health conditions can be very difficult to diagnose. That is why wer suggest always seeking additional opinions.
+            Below are a few additional assesment tools that can help you get a grasp on your mental health: 
+
+            <br/>
+            <ul>
+                <li> <a href="https://doctorondemand.com/what-we-treat/anxiety/?utm_source=search&utm_medium=cpc&utm_campaign=3Q_Google_Search_DOD_BH-Conditions_Gamma&utm_term=12616946&gclid=CjwKCAiAsIGrBhAAEiwAEzMlCwUrThbifdUtve3Avv639QGFdFpThdlqNNrz5Kp-e2TVoYHpwPAJGBoC9VwQAvD_BwE"> Doctor On Demand </a> : An online therapy resource </li>
+                <li> <a href="https://www.heretohelp.bc.ca/screening/online/?screen=depression"> Here to Help </a>: An online depression screening tool </li>
+                <li> <a href="https://www.mdcalc.com/calc/1725/phq9-patient-health-questionnaire9"> PHQ-9 </a>: A link to the Patient Health Questionnaire-9 </li>
+            </ul>
+        </p>
+
+        <br/>
+        <p class="treatment"> 3) Brief Mindfulness Exercise </p>
+
+        <p class="description"> Below is a brief video that explains a mindfulness exercise that we hope you find useful: </p>
+        <center><iframe width="840" height="630"
+            src="https://www.youtube.com/embed/bcW6HOJr5Cg?si=IQZa55O6ESHxW_eW">
+        </iframe></center>
+    </div>
+    );
+}
+
+export default App;
